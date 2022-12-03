8502688948ac1ba170bf9d02dde797379da84f5b
GladysKigen <gladys.kigen@student.moringa.com>
2022-12-03 15:32:31 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", () =>
+  console.log("The DOM has Loaded")
+);
+//console.log(
+   // "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+  //);
+
+document.addEventListener("DOMContentLoaded", function () {
+let p = document.querySelector("p");
+p.textContent = "This is really cool!";
+});
